cd016e9902fcaf54a52296eb436113d2c13d9a30
skyhe <skyhe@VIBO>
2025-01-19 17:45:36 +0200

    Add project files.

---
+++ Diplomna_viktor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Diplomna_viktor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
