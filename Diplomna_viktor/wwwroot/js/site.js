9af2813c52d8adb2cbe6011e5aac9f1fadd2c0b0
Viktor Pozharliev <skyhero31@abv.bg>
2025-02-06 02:02:42 +0200

    fixed a lot of things

--- Diplomna_viktor/wwwroot/js/site.js
+++ Diplomna_viktor/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
+
+document.querySelectorAll('.like-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        const isLiked = button.classList.toggle('liked'); // Добавя/премахва клас 'liked'
+        const svgIcon = button.querySelector('svg');
+        if (isLiked) {
+            svgIcon.style.stroke = 'red'; // Запълва сърцето
+        } else {
+            svgIcon.style.stroke = 'black'; // Връща го към черно
+        }
+    });
+});
